12f21e8e60c244e33795c13c07428eb2a51732bb
Ignacio Yuste Lopez <iyustlop@gmail.com>
2022-04-12 15:20:17 +0200

    creation of the types

---
+++ src/services/diaries.ts
@@ -0,0 +1,7 @@
+import { DiaryEntry } from '../types'
+import diariesData from './diaries.json'
+
+const diaries: Array<DiaryEntry> = diariesData as Array<DiaryEntry>
+export const getEntries = () =>  diaries
+
+export const addEntry = () => null
